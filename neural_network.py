fd990b86d12a092dcf9ff01ecdbeea5adc31d2f8
cian74 <cianbarrett3011@gmail.com>
2024-10-28 20:17:00 +0000

    can make predictions w 90% acc

--- neural_network.py
+++ neural_network.py
@@ -11,28 +11,31 @@ np.random.shuffle(data)
 data_dev = data[0:1000].T
 Y_dev = data_dev[0]
 X_dev = data_dev[1:n]
+X_dev = X_dev / 255.
 
 data_train = data[1000:m].T
 Y_train = data_train[0]
 X_train = data_train[1:n]
+X_train = X_train /255.
 
 #print(X_train[:, 0].shape)
 
 def init_params():
-    W1 = np.random.rand(10, 784)
-    b1 = np.random.rand(10, 1)
-    W2 = np.random.rand(10,10)
-    b2 = np.random.rand(10, 1)
+    W1 = np.random.rand(10, 784) * 0.01
+    b1 = np.zeros((10, 1))
+    W2 = np.random.rand(10,10)* 0.01
+    b2 = np.zeros((10, 1))
     return W1,b1,W2,b2
 
 def ReLu(Z):
-    return Z * (Z > 0)
+    return np.maximum(0,Z) 
 
 def dReLu(Z):
-    return 1 if Z > 0 else 0
+    return Z > 0 
 
 def softmax(Z):
-    A = np.exp(Z) / sum(np.exp(Z))
+    Z -= np.max(Z, axis=0)  # Subtract max value for numerical stability
+    A = np.exp(Z) / np.sum(np.exp(Z), axis=0)
     return A
 
 def forward_propagation(W1, b1, W2, b2, X):
@@ -42,3 +45,47 @@ def forward_propagation(W1, b1, W2, b2, X):
     A2 = softmax(Z2)
     return Z1, A1, Z2, A2
 
+def one_hot(Y):
+    one_hot_Y = np.zeros((Y.size, Y.max() + 1))
+    one_hot_Y[np.arange(Y.size), Y] = 1
+    one_hot_Y = one_hot_Y.T
+    return one_hot_Y
+
+def backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y):
+    one_hot_Y = one_hot(Y)
+    dZ2 = A2 - one_hot_Y
+    dW2 = 1 / m * dZ2.dot(A1.T)
+    dZ1 = W2.T.dot(dZ2) * dReLu(Z1)
+    dW1 = 1 / m * dZ1.dot(X.T)
+    db2 = 1 / m * np.sum(dZ2, axis=1, keepdims=True)
+    db1 = 1 / m * np.sum(dZ1, axis=1, keepdims=True)
+
+    return dW1, db1, dW2, db2
+
+def update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha):
+    W1 = W1 - alpha * dW1
+    b1 = b1 - alpha * db1
+    W2 = W2 - alpha * dW2
+    b2 = b2 - alpha * db2
+    return W1, b1, W2, b2
+
+def get_predictions(A2):
+    return np.argmax(A2, 0)
+
+def get_accuracy(predictions, Y):
+    print(predictions, Y)
+    return np.sum(predictions == Y) / Y.size
+
+def gradient_descent(X, Y, alpha, iterations):
+    W1, b1, W2, b2 = init_params()
+    for i in range(iterations):
+        Z1, A1, Z2, A2 = forward_propagation(W1, b1, W2, b2, X)
+        dW1, db1, dW2, db2 = backward_prop(Z1, A1, Z2, A2, W1, W2, X, Y)
+        W1, b1, W2, b2 = update_params(W1, b1, W2, b2, dW1, db1, dW2, db2, alpha)
+        if (i % 10 == 0):
+            print("Iteration: ", i)
+            predictions = get_predictions(A2)
+            print(get_accuracy(predictions, Y))
+    return W1, b1, W2, b2
+
+W1, b1, W2, b2 = gradient_descent(X_train, Y_train, alpha=0.1, iterations=100)
